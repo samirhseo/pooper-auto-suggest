a04dd8ba12918934079fd96be06af437349524af
SamirHamrouni <samir.hamrouni@searchlaboratory.com>
2022-07-12 10:06:03 +0100

    added csv upload. Requires a csv upload with a header row and a single column of keywords.

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ with st.form("my_form"):
     term = st.text_input('Enter a search term', placeholder='Online Poker')
     submitted = st.form_submit_button("Submit")
 
+
+with st.form("bulk_submit"):
+    bulk_term = st.file_uploader("Upload CSV Here",key=1)
+    bulk_submitted = st.form_submit_button("Submit")
+
 if submitted:
     download = []
     data = {}
@@ -43,3 +48,36 @@ if submitted:
         st.header(key.replace('*', '-'))
         for value in values:
             st.write(value)
+
+if bulk_submitted:
+    download = []
+    data = {}
+    variations = ['what * ', 'is * ', 'who * ', 'how * ', 'does * ', 'why * ', 'can * ', 'where * ', 'when * ', '* ']
+    bulk_term_data = pd.read_csv(bulk_term)
+    print(bulk_term_data.iloc[0:-1,0])
+
+    for term in bulk_term_data.iloc[:,-1]:
+        print(term)
+        for variant in variations:
+            response = json.loads(
+                requests.get(f'http://suggestqueries.google.com/complete/search?client=firefox&q={variant}{term}').text)
+            data[response[0]] = [i for i in response[1]]
+
+    for _, values in data.items():
+        for value in values:
+            download.append(value)
+
+    df = pd.DataFrame({'suggest': download})
+    csv = convert_df(df)
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name=f'google_auto_suggest_{term}.csv',
+        mime='text/csv',
+    )
+
+    for key, values in data.items():
+        st.header(key.replace('*', '-'))
+        for value in values:
+            st.write(value)
+
